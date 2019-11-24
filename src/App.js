2482bdfb8cdfe4df1e9d333a006fc0167406b11a
Barak Dimand <barakdimand@aol.com>
2019-11-24 23:28:59 +0200

    added a working select animal

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from "react";
 import { render } from "react-dom";
+import SearchParams from "./SearchParams";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "kiwi",
-      animal: "bird",
-      breed: "cockatiel"
-    }),
-    React.createElement(Pet, {
-      name: "milo",
-      animal: "dog",
-      breed: "beagle"
-    }),
-    React.createElement(Pet, {
-      name: "bubbles",
-      animal: "cat",
-      breed: "fat"
-    })
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <SearchParams />
+      {/* <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Pet name="Kiwi" animal="bird" breed="cockatiel" />
+      <Pet name="milo" animal="dog" breed="beagle" />
+      <Pet name="Luna" animal="dog" breed="havanese" /> */}
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
+
+// return React.createElement("div", {}, [
+//   React.createElement("h1", {}, "Adopt Me!"),
+//   React.createElement(Pet, {
+//     name: "kiwi",
+//     animal: "bird",
+//     breed: "cockatiel"
+//   }),
+//   React.createElement(Pet, {
+//     name: "milo",
+//     animal: "dog",
+//     breed: "beagle"
+//   }),
+//   React.createElement(Pet, {
+//     name: "bubbles",
+//     animal: "cat",
+//     breed: "fat"
+//   })
+// ]);
